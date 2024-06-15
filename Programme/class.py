1eecf647a1de27505bc2b51a87e4266c0c9a9b51
Ishem <123887246+IshemZ@users.noreply.github.com>
2024-06-15 21:58:19 +0200

    Ajout de la classe Voyageur et Destination

--- Programme/class.py
+++ Programme/class.py
@@ -1,8 +1,26 @@
 class Voyageur: #nom de la classe
     
     def __init__ (self, nom, age): #constructeur init avec les attributs de la classe
-        
         self.nom = nom 
         self.age = age
+
+    def afficher_info(self):
+        print(f"Le nom du voyageur est {self.nom} et il a {self.age} ans.")
+        
+
+
     
-voyageur1 = Voyageur.nom()
+    
+    
+class Destination:
+
+    def __init__(self, ville, activite):
+        self.ville = ville
+        self.activite = activite
+        
+        
+    def ajout_activite(self):
+        self.activite
+    
+    
+#Voyageur1 = Voyageur("Conrad", 24)
