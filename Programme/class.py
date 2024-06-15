bbbbf97f7f4a2388d47600a33cd4982a524ab396
Ishem <123887246+IshemZ@users.noreply.github.com>
2024-06-15 22:34:16 +0200

    test des fonctionnalité de la class Destination

--- Programme/class.py
+++ Programme/class.py
@@ -8,19 +8,30 @@ class Voyageur: #nom de la classe
         print(f"Le nom du voyageur est {self.nom} et il a {self.age} ans.")
         
 
+Voyageur1 = Voyageur("Conrad", 24)
 
-    
-    
-    
+Voyageur1.afficher_info()
+
+
+
 class Destination:
 
-    def __init__(self, ville, activite):
+    def __init__(self, ville):
         self.ville = ville
-        self.activite = activite
-        
-        
-    def ajout_activite(self):
-        self.activite
-    
-    
-#Voyageur1 = Voyageur("Conrad", 24)
+        self.activite = []
+
+    def ajout_activite(self, activite):
+        self.activite.append(activite)
+
+    def afficher_destination(self):
+        activite_str = ", ".join(self.activite)            
+        print(f"{self.ville} est la ville de destination ou vous pourrez {activite_str} ")
+
+
+Destination1 = Destination("Paris")
+Destination1.ajout_activite("Visiter la Tour-Eiffel")
+Destination1.ajout_activite("Prendre un verre dans une terrasse")
+Destination1.afficher_destination()
+
+
+
