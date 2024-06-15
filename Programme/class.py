439a9ed4fcf4a8e8aeb17474eab173275c627efe
Ishem <123887246+IshemZ@users.noreply.github.com>
2024-06-15 20:42:58 +0200

    first commit

---
+++ Programme/class.py
@@ -0,0 +1,8 @@
+class Voyageur: #nom de la classe
+    
+    def __init__ (self, nom, age): #constructeur init avec les attributs de la classe
+        
+        self.nom = nom 
+        self.age = age
+    
+voyageur1 = Voyageur.nom()
